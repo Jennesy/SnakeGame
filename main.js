d35d81a1db6e8a506307af661b20fd5f76039dd2
Jennesy <jennesy.dai@gmail.com>
2022-06-02 18:54:04 +0800

    fix: restart router link

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let gameOver = false
 function main(currentTime) {
 	if (gameOver) {
 		if (confirm('You lost. Press ok to restart.')) {
-			window.location = '/'
+			window.location = '/SnakeGame'
 		}
 		return
 	}
