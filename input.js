dc6bbbbd6aa7f3208f6c1f5218377a48f0568fb2
Jennesy <jennesy.dai@gmail.com>
2022-06-03 12:59:13 +0800

    feat: Add arrow buttons for no-keyboard devices

--- input.js
+++ input.js
@@ -23,6 +23,31 @@ window.addEventListener('keydown', (event) => {
 	}
 })
 
+document.querySelectorAll('.button').forEach((button) => {
+	button.addEventListener('click', (event) => {
+		console.log(event.target)
+		lastInputDirection = inputDirection
+		switch (event.target.id) {
+			case 'ArrowUp':
+				if (lastInputDirection.y !== 0) break
+				inputDirection = { x: 0, y: -1 }
+				break
+			case 'ArrowDown':
+				if (lastInputDirection.y !== 0) break
+				inputDirection = { x: 0, y: 1 }
+				break
+			case 'ArrowLeft':
+				if (lastInputDirection.x !== 0) break
+				inputDirection = { x: -1, y: 0 }
+				break
+			case 'ArrowRight':
+				if (lastInputDirection.x !== 0) break
+				inputDirection = { x: 1, y: 0 }
+				break
+		}
+	})
+})
+
 export function getInputDirection() {
 	return inputDirection
 }
