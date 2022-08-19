66ad23f2eaae91379f8ce50c741bec1cc9ab24fe
Jennesy <jennesy.dai@gmail.com>
2022-08-19 18:08:33 +0800

    feat: Change to touch-control for no-keyboard devices

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
 
-window.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (event) => {
 	lastInputDirection = inputDirection
 	switch (event.key) {
 		case 'ArrowUp':
@@ -23,28 +23,36 @@ window.addEventListener('keydown', (event) => {
 	}
 })
 
-document.querySelectorAll('.button').forEach((button) => {
-	button.addEventListener('click', (event) => {
-		console.log(event.target)
-		lastInputDirection = inputDirection
-		switch (event.target.id) {
-			case 'ArrowUp':
-				if (lastInputDirection.y !== 0) break
-				inputDirection = { x: 0, y: -1 }
-				break
-			case 'ArrowDown':
-				if (lastInputDirection.y !== 0) break
-				inputDirection = { x: 0, y: 1 }
-				break
-			case 'ArrowLeft':
-				if (lastInputDirection.x !== 0) break
+const touches = {}
+document.addEventListener('touchstart', (event) => {
+	event.preventDefault()
+	;[...event.changedTouches].forEach((touch) => {
+		touches[touch.identifier] = { startX: touch.screenX, startY: touch.screenY }
+	})
+})
+
+document.addEventListener('touchmove', (event) => {
+	event.preventDefault()
+})
+
+document.addEventListener('touchend', (event) => {
+	lastInputDirection = inputDirection
+	;[...event.changedTouches].forEach((touch) => {
+		const moveX = touch.screenX - touches[touch.identifier].startX
+		const moveY = touch.screenY - touches[touch.identifier].startY
+		if (Math.abs(moveX) > Math.abs(moveY)) {
+			if (moveX < 0 && lastInputDirection.x === 0)
 				inputDirection = { x: -1, y: 0 }
-				break
-			case 'ArrowRight':
-				if (lastInputDirection.x !== 0) break
+			if (moveX > 0 && lastInputDirection.x === 0)
 				inputDirection = { x: 1, y: 0 }
-				break
 		}
+		if (Math.abs(moveY) > Math.abs(moveX)) {
+			if (moveY < 0 && lastInputDirection.y === 0)
+				inputDirection = { x: 0, y: -1 }
+			if (moveY > 0 && lastInputDirection.y === 0)
+				inputDirection = { x: 0, y: 1 }
+		}
+		delete touches[touch.identifier]
 	})
 })
 
